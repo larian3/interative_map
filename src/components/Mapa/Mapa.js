62973bca396597b0705319ef6e483712048766e3
lariane <larianelima22@gmail.com>
2025-04-07 10:01:25 -0300

    foco no estado do Pará e resolvendo erro de window

--- src/components/Mapa/Mapa.js
+++ src/components/Mapa/Mapa.js
@@ -4,11 +4,7 @@ import { MapContainer, TileLayer } from "react-leaflet";
 const Mapa = () => {
   return (
     <div className="h-full w-full">
-      <MapContainer
-        center={[-23.5505, -46.6333]}
-        zoom={13}
-        className="h-full w-full z-0"
-      >
+      <MapContainer center={[-3.4168, -52.1472]} zoom={6} className="h-full w-full z-0">
         <TileLayer
           attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a>'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
